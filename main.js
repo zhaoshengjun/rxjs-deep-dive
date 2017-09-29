8e201f90b55371e073ee53fb6ba1c7dd7dd4259c
Sam Zhao <zhao.shengjun@gmail.com>
2017-09-29 14:21:13 +1000

    from operator

--- main.js
+++ main.js
@@ -1,23 +1,18 @@
-// import Rx from "rxjs/Rx";
 import { Observable } from "rxjs/Observable";
-import "rxjs/add/operator/map";
+// import { of } from "rxjs/add/observable/of";
+import { from } from "rxjs/add/observable/from";
 
-// Observerable
-let obs1 = Observable.create(sub => {
-  sub.next(1);
-  sub.next(2);
-  sub.next(3);
-  sub.complete();
-});
+const log = val => console.log(val + "!");
 
-// Observer / Subscriber / Subscription
+// of
+// Observable.of("React", "Angular 2", "Meteor").subscribe(log);
+// React
+// Angular 2
+// Meteor
 
-obs1.subscribe({
-  next: console.log,
-  error: console.error,
-  complete: () => console.log("done")
-});
-
-// Operator
-let obs2 = obs1.map(x => x + 100);
-obs2.subscribe(console.log);
+// from
+Observable.from(["poe", "shlley", "bronte", "eliot"]).subscribe(log);
+// poe
+// shlley
+// bronte
+// eliot
