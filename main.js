ab807b0735a3edd2d425756037162d2a52152755
Sam Zhao <zhao.shengjun@gmail.com>
2017-09-29 14:33:53 +1000

    merge

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import { Observable } from "rxjs/Observable";
-import { bindNodeCallback } from "rxjs/add/observable/bindNodeCallback";
+import { interval } from "rxjs/add/observable/interval";
+import "rxjs/add/operator/map";
+import "rxjs/add/observable/merge";
 
 const log = val => console.log(val);
 
-const fetch = (url, callback) => {
-  callback(null, "data");
-};
+const o1 = Observable.interval(1000).map(x => "a" + x);
+const o2 = Observable.interval(2000).map(x => "b" + x);
+const o3 = Observable.interval(3000).map(x => "c" + x);
 
-const rxFetch = Observable.bindNodeCallback(fetch);
-rxFetch("url").subscribe(log);
+Observable.merge(o1, o2, o3).subscribe(log);
+// whenever there's a value, the observable will emit.
