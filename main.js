f3e59f2a01479311f3c99961489bab55e08e5634
Sam Zhao <zhao.shengjun@gmail.com>
2017-09-29 15:08:37 +1000

    zip

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { Observable } from "rxjs/Observable";
 import { interval } from "rxjs/add/observable/interval";
 import "rxjs/add/operator/map";
-import "rxjs/add/observable/concat";
+import "rxjs/add/observable/zip";
 
 const log = val => console.log(val);
 
@@ -9,5 +9,5 @@ const o1 = Observable.interval(1000).map(x => "a" + x);
 const o2 = Observable.interval(2000).map(x => "b" + x);
 const o3 = Observable.interval(3000).map(x => "c" + x);
 
-Observable.concat(o1, o2).subscribe(log);
-// o2 will never start until o1 is complete
+Observable.zip(o1, o2, o3).subscribe(log);
+// make sure every observable to have it's value
