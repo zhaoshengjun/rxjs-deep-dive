16a96c5ebf68810722d090fd11380fa971442b8a
Sam Zhao <zhao.shengjun@gmail.com>
2017-09-29 14:42:57 +1000

    concat

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { Observable } from "rxjs/Observable";
 import { interval } from "rxjs/add/observable/interval";
 import "rxjs/add/operator/map";
-import "rxjs/add/observable/merge";
+import "rxjs/add/observable/concat";
 
 const log = val => console.log(val);
 
@@ -9,5 +9,5 @@ const o1 = Observable.interval(1000).map(x => "a" + x);
 const o2 = Observable.interval(2000).map(x => "b" + x);
 const o3 = Observable.interval(3000).map(x => "c" + x);
 
-Observable.merge(o1, o2, o3).subscribe(log);
-// whenever there's a value, the observable will emit.
+Observable.concat(o1, o2).subscribe(log);
+// o2 will never start until o1 is complete
